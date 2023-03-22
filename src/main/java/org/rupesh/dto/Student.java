20602c6702fa8345d51e10a63adf86888122bc75
9640678255 <rupeshkumar0569@gmail.com>
2023-03-22 20:59:22 +0530

    new one is added

--- src/main/java/org/rupesh/dto/Student.java
+++ src/main/java/org/rupesh/dto/Student.java
@@ -2,18 +2,31 @@ package org.rupesh.dto;
 
 import java.io.Serializable;
 
-public class Student implements Serializable{
+public class Student implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 520172259171006138L;
-	
+
 	private String id;
 	private String name;
 	private int age;
 	private String gender;
-
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id=id;
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 
 }
