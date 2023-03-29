f577ce15fdbc4f9a009dcb0bff8d493dca4d83c2
9640678255 <rupeshkumar0569@gmail.com>
2023-03-29 11:56:49 +0530

    added EvenOrOdd

---
+++ src/main/java/EvenOrOdd.java
@@ -0,0 +1,12 @@
+
+public class EvenOrOdd {
+	public static void main(String[] args) {
+		int x=9;
+		if(x%2==0)
+		{
+			System.out.println(x+"even number");
+		}
+		else
+		{
+			System.out.println(x+"odd number");
+		}}}
