6e29ecbfa1dae18805fb978b0da7703e85f89e9a
9640678255 <rupeshkumar0569@gmail.com>
2023-03-31 16:11:52 +0530

    added automorphic number code

---
+++ src/main/java/Automorphic.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Automorphic {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int sq=a*a;
+		System.out.println(sq);
+		int temp=sq;
+		while(temp!=0)
+		{
+			int rem=temp%10;
+			if(rem==a) {
+				System.out.println(a+" is aautomorphic number");
+				break;
+				
+			}
+			
+			
+		}
+	}
+
+}
